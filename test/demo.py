5d3a43c99d5b4df4429f2ec73acfb5dfdb650175
User <sdi1900106@di.uoa.gr>
2021-10-24 22:46:30 +0300

    test

--- test/demo.py
+++ test/demo.py
@@ -1,4 +1,9 @@
-ap=["banana","apple"]
-for i in ap:
-    print(i)
-print(i)
+guests = ["Alice", "Bob", "Carol", "David" ]
+invites = [1] * 3 + [0]
+def is_invited(guest, inv, no):
+   if inv:
+      print(f"Guest {no}: Welcome, {guest}")
+   else:
+      print(f"Guest {no}: You are not invited, {guest}")
+for no, (guest, inv) in enumerate(zip(guests, invites)):
+   is_invited(guest, inv, no)
